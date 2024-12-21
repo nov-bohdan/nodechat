eed523940e424e834c26f0b72e572982b4b7ed66
nov-bohdan <nov.bohdan@gmail.com>
2024-12-20 18:14:20 -0600

    OnlineUsers object added to socket.js

--- socket.js
+++ socket.js
@@ -1,8 +1,32 @@
 const { Server } = require("socket.io");
 const { verifyUser, onMessage, getOnlineUsers } = require("./socket_util");
 
+const OnlineUsers = {
+  usersList: [],
+  findUserByUsername(username) {
+    const user = this.usersList.find(
+      (userItem) => userItem.user.username === username
+    );
+    return user;
+  },
+  deleteUserByUsername(username) {
+    this.usersList = this.usersList.filter(
+      (userItem) => userItem.user.username !== username
+    );
+  },
+  addNewUser(user, socket) {
+    const newUser = {
+      user,
+      socket,
+    };
+    this.usersList.push(newUser);
+  },
+  getAllUsernames() {
+    return this.usersList.map((user) => user.user.username);
+  },
+};
+
 exports.newSocket = (server) => {
-  const onlineUsers = new Set();
   const io = new Server(server, {
     cors: {
       origin: "http://localhost:5173",
@@ -14,15 +38,17 @@ exports.newSocket = (server) => {
   io.on("connection", (socket) => {
     const username = socket.user.username;
     console.log(`${username} connected!`);
-    onlineUsers.add(username);
-    io.emit("online_users", [...onlineUsers]);
+    OnlineUsers.addNewUser(socket.user, socket);
+    const users = OnlineUsers.getAllUsernames();
+    io.emit("online_users", users);
 
     socket.on("disconnect", () => {
       console.log(`${username} disconnected`);
-      if (onlineUsers.has(username)) {
-        onlineUsers.delete(username);
+      if (OnlineUsers.findUserByUsername(username)) {
+        OnlineUsers.deleteUserByUsername(username);
       }
-      io.emit("online_users", [...onlineUsers]);
+      const users = OnlineUsers.getAllUsernames();
+      io.emit("online_users", users);
     });
 
     socket.on("message", (message) => {
